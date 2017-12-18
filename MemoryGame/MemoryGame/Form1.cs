58f41499f1faed78a9fe3db982755e6a97ed19c5
Jordan Kennedy <G00348844@gmit.ie>
2017-12-18 18:20:25 +0000

    Random Item Generation

--- MemoryGame/MemoryGame/Form1.cs
+++ MemoryGame/MemoryGame/Form1.cs
@@ -12,6 +12,14 @@ namespace MemoryGame
 {
     public partial class Form1 : Form
     {
+        Random random = new Random();
+
+        List<string> icons = new List<string>()
+        {
+            "!","!","N","N",",",",","k","k",
+            "b","b","w","w","v","v","z","z"
+        };
+
         public Form1()
         {
             InitializeComponent();
